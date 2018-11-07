ecf58dddd430058d19cea55efcbfef9e40363bb8
LAPTOP-4A79OJ3C\jordi <jordidewaard97@gmail.com>
2018-11-07 17:01:25 +0100

    Pokemon kan nu doodgaan (Geen - getallen meer)

--- Pokemon.php
+++ Pokemon.php
@@ -9,7 +9,8 @@ class Pokemon
     public $Weakness;
     public $Resistance;
 
-    public function __construct($nm, $healthp, $hitp, $et) {
+    public function __construct($nm, $healthp, $hitp, $et)
+    {
         $this->Name = $nm;
         $this->EnergyType = $et;
         $this->Health = $healthp;
@@ -17,36 +18,39 @@ class Pokemon
         $this->Attack = [];
     }
 
-    function DoAttack($pokemon, $pokemon2, $target) {
+    function DoAttack($pokemon, $pokemon2, $target)
+    {
         echo $this->Name. ' Will attack ' . $target->Name . ' Using ' . $this->Attack[0]->Name;
         echo '<br>';
         if ($pokemon->EnergyType == $pokemon2->Weakness->EnergyType) {
             $pok2nh = $pokemon2->Health - ($pokemon->Attack[0]->AttackPoints * $pokemon2->Weakness->Multiplier);
-            echo $pokemon2->Name . " Now Has " . $pok2nh . " HP Left";
-        }
-        else {
+        } else {
             $pok2nh = $pokemon2->Health - $pokemon->Attack[0]->AttackPoints;
+        }
+
+        if ($pok2nh < 1) {
+            echo $pokemon2->Name . " Has Died ";
+        } else {
             echo $pokemon2->Name . " Now Has " . $pok2nh . " HP Left";
         }
     }
 
-    function DoAttack2($pokemon2, $pokemon, $target) {
+    function DoAttack2($pokemon2, $pokemon, $target)
+    {
         echo $this->Name. ' Will attack ' . $target->Name . ' Using ' . $this->Attack[1]->Name;
         echo '<br>';
         if ($pokemon2->EnergyType == $pokemon->Weakness->EnergyType) {
             $poknh = $pokemon->Health - ($pokemon2->Attack[1]->AttackPoints * $pokemon->Weakness->Multiplier);
-            echo $pokemon->Name . " Now Has " . $poknh . " HP Left";
-        }
-        else {
+        } else {
             $poknh = $pokemon->Health - $pokemon2->Attack[1]->AttackPoints;
+        }
+
+        if ($poknh < 1) {
+            echo $pokemon->Name . " Has Died ";
+        } else {
             echo $pokemon->Name . " Now Has " . $poknh . " HP Left";
         }
     }
-
-
-
-
-
 }
 
 
--- index.php
+++ index.php
@@ -1,21 +1,18 @@
 <?php
 
-require 'Pokemon.php';
-require 'Attack.php';
-require 'Resistance.php';
-require 'Weakness.php';
+require 'Pokemon.php'; require 'Attack.php'; require 'Resistance.php'; require 'Weakness.php';
 
 $pokemon = new Pokemon("Pikachu", 60, 60, "Lightning");
-$pokemon->Weakness = new Weakness("Fire", 1.5);
-$pokemon->Resistance = new Resistance("Fighting", 20);
 $pokemon->Attack[] = new Attack("Electric Ring", 50);
 $pokemon->Attack[] = new Attack("Pika Punch", 20);
+$pokemon->Weakness = new Weakness("Fire", 1.5);
+$pokemon->Resistance = new Resistance("Fighting", 20);
 
 $pokemon2 = new Pokemon("Charmeleon", 60, 60, "Fire");
-$pokemon2->Weakness = new Weakness("Water", 2);
-$pokemon2->Resistance = new Resistance("Fire", 10);
 $pokemon2->Attack[] = new Attack("Head Butt", 10);
 $pokemon2->Attack[] = new Attack("Flare", 30);
+$pokemon2->Weakness = new Weakness("Water", 2);
+$pokemon2->Resistance = new Resistance("Fire", 10);
 
 echo $pokemon2->Name . " HP " . $pokemon2->Health . "/" . $pokemon2->Hitpoints;
 echo '<br>';
@@ -26,4 +23,3 @@ $pokemon->DoAttack($pokemon, $pokemon2, $pokemon2);
 echo '<br>';
 $pokemon2->DoAttack2($pokemon2, $pokemon, $pokemon);
 echo '<br>';
-?>
